237f99ba36eecd76a5b72ebe756c2319557c06de
iLiranS <lirangamerz@gmail.com>
2026-04-02 19:55:42 +0300

    fix:cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Apply these headers only to your changelog
+                source: '/changelog.json',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
+                ],
+            },
+        ];
+    },
+};
 
-export default nextConfig
+export default nextConfig; // or module.exports = nextConfig;
